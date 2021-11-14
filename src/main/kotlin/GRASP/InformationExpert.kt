3173501ee8a2bfd109aec81a15fca9342e7b1ba5
Nail Asadullin <latenightgoat@gmail.com>
2021-11-14 19:00:20 +0300

    added information expert example

--- src/main/kotlin/GRASP/InformationExpert.kt
+++ src/main/kotlin/GRASP/InformationExpert.kt
@@ -2,18 +2,18 @@ package GRASP
 
 /**
  * Information expert
-See also: Information hiding
-
-Problem: What is a basic principle by which to assign responsibilities to objects?
-Solution: Assign responsibility to the class that has the information needed to fulfill it.
-
-Information expert (also expert or the expert principle) is a principle used to determine where to delegate
-responsibilities such as methods, computed fields, and so on.
-
-Using the principle of information expert, a general approach to assigning responsibilities is to look at a given
-responsibility, determine the information needed to fulfill it, and then determine where that information is stored.
-
-This will lead to placing the responsibility on the class with the most information required to fulfill it.
+ * See also: Information hiding
+ *
+ * Problem: What is a basic principle by which to assign responsibilities to objects?
+ * Solution: Assign responsibility to the class that has the information needed to fulfill it.
+ *
+ * Information expert (also expert or the expert principle) is a principle used to determine where to delegate
+ * responsibilities such as methods, computed fields, and so on.
+ *
+ * Using the principle of information expert, a general approach to assigning responsibilities is to look at a given
+ * responsibility, determine the information needed to fulfill it, and then determine where that information is stored.
+ *
+ * This will lead to placing the responsibility on the class with the most information required to fulfill it.
  * */
 abstract class InformationExpertExample() {
     val orders = FakeRepo().orders
@@ -37,7 +37,7 @@ class Wrong(): InformationExpertExample() {
 }
 
 /**
- * Good realisation:
+ * Good realisation: class delegates computing to inner classes
  * */
 class Ok(): InformationExpertExample() {
     override fun getSum(): Int {
