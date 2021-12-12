35a97754fc39c7af23668b692a4fea3fbd140435
NightGoat <latenightgoat@gmail.com>
2021-12-12 19:22:35 +0300

    reformatted cafe.kt

--- src/main/kotlin/gof/abstract_factory/Cafe.kt
+++ src/main/kotlin/gof/abstract_factory/Cafe.kt
@@ -11,13 +11,18 @@ import gof.abstract_factory.models.Customer
 import java.util.*
 
 class Cafe(
-    private val menus: List<Int> = listOf(JAPANESE_CUISINE_MENU_NUMBER, AMERICAN_CUISINE_MENU_NUMBER, UKRAINIAN_CUISINE_MENU_NUMBER),
+    private val menus: List<Int> = listOf(
+        JAPANESE_CUISINE_MENU_NUMBER,
+        AMERICAN_CUISINE_MENU_NUMBER,
+        UKRAINIAN_CUISINE_MENU_NUMBER
+    ),
     private val workingInterval: IntRange = IntRange(10, 21),
 ) {
     private val servedCustomers = mutableListOf<Customer>()
 
+    //not private for tests
     val isAllCustomersHappy
-        get() = servedCustomers.isEmpty() || servedCustomers.all { !it.isHungry}
+        get() = servedCustomers.isEmpty() || servedCustomers.all { !it.isHungry }
 
     private var currentHour: Int = Calendar.HOUR_OF_DAY
     private val isWorkingHours
@@ -26,22 +31,22 @@ class Cafe(
     private val door = Door()
 
     fun work(worksEndCallback: () -> Unit) {
-            while (isWorkingHours && isAllCustomersHappy) {
-                val customer = door.letCustomerIn()
-                customer?.run {
-                    val order = selectMenu(menus)
-                    val cuisine = selectCuisine(order)
-                    val lunch = cuisine.cookLunch()
-                    receiveOrder(lunch)
-                    servedCustomers.add(this)
-                    currentHour+=1
-                }
+        while (isWorkingHours && isAllCustomersHappy) {
+            val customer = door.letCustomerIn()
+            customer?.run {
+                val order = selectMenu(menus)
+                val cuisine = selectCuisine(order)
+                val lunch = cuisine.cookLunch()
+                receiveOrder(lunch)
+                servedCustomers.add(this)
+                currentHour += 1
             }
+        }
         worksEndCallback()
     }
 
     private fun selectCuisine(menu: Int): CuisineFactory {
-        return when(menu) {
+        return when (menu) {
             JAPANESE_CUISINE_MENU_NUMBER -> JapaneseCuisine
             AMERICAN_CUISINE_MENU_NUMBER -> AmericanCuisine
             UKRAINIAN_CUISINE_MENU_NUMBER -> UkrainianCuisine
